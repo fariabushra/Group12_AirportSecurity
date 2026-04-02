773b4c6b401784f206182e2a8eb32fd8c897ab17
2230251-tahasin <2230251@iub.edu.bd>
2026-04-03 03:22:39 +0600

    created a package named it Tabib

---
+++ src/main/java/com/example/airportsecurity/Tabib/ITAdministratorDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.airportsecurity.Tabib;
+
+import javafx.event.ActionEvent;
+
+public class ITAdministratorDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void arbpButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cmaButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void barButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void valButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void hisfButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void giarButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void msuButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void mshButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.airportsecurity {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.airportsecurity to javafx.fxml;
+    opens com.example.airportsecurity.Tabib to javafx.fxml;
+
     exports com.example.airportsecurity;
+    exports com.example.airportsecurity.Tabib;
 }
