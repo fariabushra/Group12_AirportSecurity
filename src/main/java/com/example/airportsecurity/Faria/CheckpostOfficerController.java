edea8cfa83a98285196f9ac1a5e3c6b2af029da2
fariabushra <bushrafaria4@gmail.com>
2026-04-08 00:36:52 +0600

    created checkpost officer fxml and updated controller

---
+++ src/main/java/com/example/airportsecurity/Faria/CheckpostOfficerController.java
@@ -0,0 +1,46 @@
+package com.example.airportsecurity.Faria;
+
+import javafx.event.ActionEvent;
+
+public class CheckpostOfficerController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void identityOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dutyOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void messageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void vehicleOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportActivityOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dutyStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void denyFormOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void securityAlertOnAction(ActionEvent actionEvent) {
+    }
+}
