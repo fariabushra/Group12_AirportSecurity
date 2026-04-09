1c9bf722a89205d66a0ef2a79ed78beef22811d6
Samia Habib Nondon <2311950@iub.edu.bd>
2026-04-09 17:22:42 +0600

    Cleaned project structure and finalized 8 goals

--- Group12_AirportSecurity-master/src/main/java/com/example/airportsecurity/Ahsan/CommandingofficerLoginSceneController.java
+++
@@ -1,7 +0,0 @@
-package com.example.airportsecurity.Ahsan;
-
-public class CommandingofficerLoginSceneController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,13 @@ module com.example.airportsecurity {
     requires javafx.fxml;
     requires javafx.graphics;
 
-
     opens com.example.airportsecurity to javafx.fxml;
     opens com.example.airportsecurity.Tabib to javafx.fxml;
     opens com.example.airportsecurity.Faria to javafx.fxml;
-
+    opens com.example.airportsecurity.Samia to javafx.fxml; // Make sure this folder exists!
 
     exports com.example.airportsecurity;
     exports com.example.airportsecurity.Tabib;
     exports com.example.airportsecurity.Faria;
+    exports com.example.airportsecurity.Samia;
 }
